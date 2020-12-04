4ced5d8e844c093fe0dd54d4b025ec69f8a36148
David Hansson <x.david.hansson@bonnierforlagen.se>
2020-12-04 15:53:32 +0100

    Added Skeleton

---
+++ AOC_05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AOC_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string realInput = @"";
+            int realResult = 0;
+
+            string testInput = @"";
+            int testResult = 0;
+
+            Console.WriteLine($"Test: {testResult} Real: {realResult} ");
+        }
+    }
+
+    class Solver
+    {
+        public int Solve(string input)
+        {
+            int result = 0;
+            return result;
+        }
+
+        public string Solve(string input)
+        {
+            string result= "";
+            return result;
+        }
+    }
+}
---
+++ Skeleton.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string realInput = @"";
+        int realResult = 0;
+
+        string testInput = @"";
+        int testResult = 0;
+
+        Console.WriteLine($"Test: {testResult} Real: {realResult} ");
+    }
+}
+
+class Solver
+{
+    public int Solve()
+    {
+        int result = 0;
+        return result;
+    }
+
+    public string Solve()
+    {
+        string result = "";
+        return result;
+    }
+}
